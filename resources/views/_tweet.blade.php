037b01d06cacd5f14f98a9e459574a40ea891149
Dani Ihza Farrosi <dani.videda@gmail.com>
2020-06-13 11:22:18 +0700

    Timeline layouting

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,14 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/50"
+            alt="avatar"
+            class="rounded-full mr-2"
+        >
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+        <p class="text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur dolore officiis perferendis commodi porro, reiciendis vero rem dolorem veniam laboriosam, delectus vitae! Omnis eius optio, inventore quod corporis sint esse itaque sit temporibus, recusandae eaque modi amet quae quis. Quas nostrum doloribus harum soluta eius, minus consequuntur repellat vero perspiciatis.</p>
+    </div>
+</div>
